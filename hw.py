b0e40a08affc6d92ab242e25761ff88e40bc939a
Raymond Zheng <163009036+snowray23@users.noreply.github.com>
2024-03-21 18:04:53 -0400

    Update hw.py

--- hw.py
+++ hw.py
@@ -1,7 +1,8 @@
 #The Range Riddle
 #task1 Write a program that prints off different moods for each day of the week.
  #"Create a list of moods such as "Happy", "Sad", "Energetic", and "Calm"
-#Using the range() function, loop through the days of the week and for each day, randomly select a mood from the list and print it
+#Using the range() function, loop through the days of the week and for each day, 
+#randomly select a mood from the list and print it
 #Ensure that your program includes the use of the random module to select the mood
 mood_list = ["Happy", "Sad", "Energetic", "Calm"]
 print(mood_list)
